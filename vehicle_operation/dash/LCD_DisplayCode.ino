ec9c76e294a8101b9a590d25d3cb44c9b33e2816
Colin A Breslow <cbreslow@umich.edu>
2022-01-21 02:09:31 +0000

    Replace LCD_DisplayCode.ino

--- vehicle_operation/dash/LCD_DisplayCode.ino
+++ vehicle_operation/dash/LCD_DisplayCode.ino
@@ -1,7 +1,7 @@
 
 // Import the Liquid Crystal library
 #include <LiquidCrystal.h>
-//Initialise the LCD with the arduino. LiquidCrystal(rs, enable, d4, d5, d6, d7)
+//Initialise the LCD with the arduino. LiquidCrystal ( rs, enable, d4, d5, d6, d7);
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
  int temperature;
@@ -12,12 +12,11 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
  int i = 0;
  bool BMS,IMD,BSPD;
 
- 
 void setup() {
- // pinMode(14, OUTPUT); //set pin 14 as output
+  pinMode(14, OUTPUT); //set pin 14 as output
   
-  //pinMode(interruptPin, INPUT_PULLDOWN);
-  //attachInterrupt(digitalPinToInterrupt(interruptPin), BMSfault, RISING);
+  pinMode(interruptPin, INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), BMSfault, RISING);
  
   lcd.begin(8, 2);// Switch on the LCD screen
   
@@ -27,36 +26,49 @@ void setup() {
   delay(1500);
 }
 
-
-
 void DisplayTempSpeed(int temperature, int speedc){
 
-  
   lcd.setCursor(0,1);
-  lcd.print("TEMP: ");
-  lcd.setCursor(6, 1);
+  lcd.print("TEMP:");
+  lcd.setCursor(5, 1);
   lcd.print(temperature);
  
-   
   lcd.setCursor(0,0);
   lcd.print("SPEED: ");
   lcd.setCursor(6, 0);
   lcd.print(speedc);
   i++;
   
-   
-
 delay(1500);
   return;
 }
 
-
 void BMSfault(){
  BFault = HIGH;
  BMS = true;
-
+}
+/*
+void TempDisplay(int temperature){
+  lcd.setCursor(0,1);
+  //lcd.autoscroll();
+  lcd.print("TEMP:");
+  lcd.setCursor(4, 1);
+  lcd.print(temperature);
+  delay(100);
+  //lcd.noAutoscroll();
+  return;
 }
 
+void SpeedDisplay(int speedc){
+  lcd.setCursor(0,0);
+  lcd.print("SPEED: ");
+  lcd.setCursor(6, 0);
+  lcd.print(speedc);
+  delay(100);
+  //autoscroll();
+  return;
+}
+*/
 void loop() {
   lcd.clear();
 
@@ -67,49 +79,28 @@ void loop() {
   DisplayTempSpeed(temperature, speedc);
   digitalWrite(14, BFault);
 
+  if(n==1){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Fault");
+      }
+    while(n==1){
+      delay(100);
+     }
 
-}
-
-if(n==1){
-
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Fault");
-}
-while(n==1){
-  delay(100);
-}
-
-
-  
-  lcd.print("10 Seconds Clock");
+  //lcd.print("10 Seconds Clock");
   for( int i = 0; i < 10; i++){
     delay(1000);
-  lcd.setCursor(i, 1);
-  lcd.setCursor(0, (1*millis()/1000));
-   print the number of seconds since reset:
+  lcd.setCursor(7, 1);
+  //lcd.setCursor(0, (1*millis()/1000));
+   //print the number of seconds since reset:
   lcd.print(i);
   lcd.print(millis()/1000);
   }
   lcd.clear();
-  lcd.print("10 Seconds");
+  lcd.setCursor(4,0);
+  lcd.print("10");
+  lcd.setCursor(1,1);
+  lcd.print("seconds");
   delay(1000);
-  
-
-void TempDisplay(int temperature){
-  lcd.setCursor(0,1);
-  lcd.print("TEMP: ");
-  lcd.setCursor(6, 1);
-  lcd.print(temperature);
-  delay(100);
-  return;
-}
-
-void SpeedDisplay(int speedc){
-  lcd.setCursor(0,0);
-  lcd.print("SPEED: ");
-  lcd.setCursor(6, 0);
-  lcd.print(speedc);
-  delay(100);
-  return;
 }
