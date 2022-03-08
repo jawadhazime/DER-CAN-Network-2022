c142ecde2245a54012bc3589bb384bb2b7be8941
Jawad Hazime <galilei@umich.edu>
2022-03-08 16:13:51 -0500

    Added voltage and temperature conversions
    temp conversion based on transfer function in thermistor datasheet;
    
    voltage range (0.5-1.75V)/analog range (39-135) correspond to
    temperature range (0-125C);
    
    should make the testing process easier.

--- sensors/thermistor/thermistor.ino
+++ sensors/thermistor/thermistor.ino
@@ -1,12 +1,29 @@
 // Testing thermistor values
+
+int sensorPin = A0;
+int sensorVal;
+float tempVal;
+float vOut;
+
 void setup() {
 	Serial.begin(115200); 
 	delay(400);
 }
 
 void loop() {
-	int sensor = analogRead(A0);
-	Serial.println(sensor);
+
+	//reads, conversions
+	sensorVal = analogRead(sensorPin);
+	vOut = 3.3 * (sensorVal/255.); //analog to voltage value
+	tempVal = (vOut - 0.5)/0.01; //transfer function solving for ambient temp
+
+	//serial prints
+	Serial.print(tempVal);
+        Serial.print("°C ");
+	Serial.print(vOut);
+	Serial.print("V ");
+	Serial.println(sensorVal);
+ 
 	delay(500); // delays sensor message to make it more readable
 }
 
