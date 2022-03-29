ff177f4390d0ec298f2a2cabbf0bf1efa0e9e246
Jawad Hazime <galilei@umich.edu>
2022-03-29 05:34:26 +0000

    Revert "Added voltage and temperature conversions"
    This reverts commit c142ecde2245a54012bc3589bb384bb2b7be8941

--- sensors/thermistor/thermistor.ino
+++ sensors/thermistor/thermistor.ino
@@ -1,29 +1,12 @@
 // Testing thermistor values
-
-int sensorPin = A0;
-int sensorVal;
-float tempVal;
-float vOut;
-
 void setup() {
 	Serial.begin(115200); 
 	delay(400);
 }
 
 void loop() {
-
-	//reads, conversions
-	sensorVal = analogRead(sensorPin);
-	vOut = 3.3 * (sensorVal/255.); //analog to voltage value
-	tempVal = (vOut - 0.5)/0.01; //transfer function solving for ambient temp
-
-	//serial prints
-	Serial.print(tempVal);
-        Serial.print("°C ");
-	Serial.print(vOut);
-	Serial.print("V ");
-	Serial.println(sensorVal);
- 
+	int sensor = analogRead(A0);
+	Serial.println(sensor);
 	delay(500); // delays sensor message to make it more readable
 }
 
